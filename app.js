1983c4f4292858303d4b0766ceb1da5be3e282e5
FloresDavid <Davidabelflores@gmail.com>
2023-10-16 23:44:25 -0300

    feat: add structure final

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const PORT = 3030;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
