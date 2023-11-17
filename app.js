c0a322eff8857f17245d73d0e682b5faf2b63327
FloresDavid <Davidabelflores@gmail.com>
2023-11-17 03:35:29 -0300

    add: login y register

--- app.js
+++ app.js
@@ -7,3 +7,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
